03202fe9787603bb1088a75405360f8d48697df1
thalesgelinger <thalesgelinger99@gmail.com>
2020-07-14 17:37:59 -0300

    feat: add game screen style

--- web/src/pages/Game/game.screen.jsx
+++ web/src/pages/Game/game.screen.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import { deck } from "../../assets"
+import "./game.css"
 
 export function GameScreen() {
+
+    const cards = [
+        "trucoBasto1",
+        "trucoBasto2",
+        "trucoBasto3"
+    ]
+
+    const cardsOponent = [
+        "notFoundCard",
+        "notFoundCard",
+        "notFoundCard"
+    ]
+
     return (
-        <h1>GameScreen</h1>
+        <div className="game-container">
+            <div className="card-container player-1">
+                {cards.map( (card, key) => (
+                    <div className="card-box">
+                        <div className="card">
+                            <img src={deck[card]} alt=""/>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="game-dropzone">
+                <div className="dropzone" aria-disabled>
+                </div>
+                <h1>Room title</h1>
+                <div className="dropzone">
+                </div>
+            </div>
+            <div className="card-container player-2">
+            {cardsOponent.map( (card, key) => (
+                    <div className="card-box">
+                        <div className="card">
+                            <img src={deck[card]} alt=""/>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
--- web/src/pages/Home/home.screen.jsx
+++ web/src/pages/Home/home.screen.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
 import "./home.css"
 
 export function HomeScreen({ match }) {
+
+    const history = useHistory()
+
+    function openGameRoom() {
+        history.push(`/game/${match.params.user}`)
+    }
+
     return (
         <div className="home-container">
             <div className="rooms-container">
-                <div className="room">
+                <div className="room" onClick={openGameRoom}>
                     <h1>Sala 01</h1>
                 </div>
             </div>
