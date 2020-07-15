d06acdb2a1a8cda8658493b6a8446eb77566a1d7
thalesgelinger <thalesgelinger99@gmail.com>
2020-07-15 05:09:16 -0300

    wip(game): add drag and drop

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-import { LoginScreen, HomeScreen, GameScreen } from './pages'
-
-
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { LoginScreen, HomeScreen, GameScreen } from "./pages";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" component={LoginScreen} exact />
-      <Route path="/home/:user" component={HomeScreen} exact />
-      <Route path="/game/:user" component={GameScreen} exact />
-    </Switch>
+    <DndProvider backend={HTML5Backend}>
+      <Switch>
+        <Route path="/" component={LoginScreen} exact />
+        <Route path="/home/:user" component={HomeScreen} exact />
+        <Route path="/game/:user" component={GameScreen} exact />
+      </Switch>
+    </DndProvider>
   );
 }
 
--- web/src/pages/Game/game.screen.jsx
+++ web/src/pages/Game/game.screen.jsx
@@ -1,48 +1,84 @@
-import React from 'react'
-import { deck } from "../../assets"
-import "./game.css"
+import React from "react";
+import { useDrag, useDrop } from "react-dnd";
+
+import { deck } from "../../assets";
+import "./game.css";
+import { useState } from "react";
 
 export function GameScreen() {
+  const [playedCard, setPlayedCard] = useState(null);
 
-    const cards = [
-        "trucoBasto1",
-        "trucoBasto2",
-        "trucoBasto3"
-    ]
+  const [cards, setCards] = useState([
+    "trucoBasto1",
+    "trucoBasto2",
+    "trucoBasto3",
+  ]);
 
-    const cardsOponent = [
-        "notFoundCard",
-        "notFoundCard",
-        "notFoundCard"
-    ]
+  const cardsOponent = ["notFoundCard", "notFoundCard", "notFoundCard"];
 
-    return (
-        <div className="game-container">
-            <div className="card-container player-1">
-                {cards.map( (card, key) => (
-                    <div className="card-box">
-                        <div className="card">
-                            <img src={deck[card]} alt=""/>
-                        </div>
-                    </div>
-                ))}
+  const [{ isDragging }, dragRef] = useDrag({
+    item: { type: "CARD" },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const [, dropRef] = useDrop({
+    accept: "CARD",
+    drop(item, monitor) {
+      console.log(item);
+      console.log(monitor);
+      moveCard();
+    },
+  });
+
+  function moveCard() {
+    const cardIndex = 2;
+
+    const cardSelected = cards.pop(cardIndex);
+
+    setPlayedCard(
+      <div>
+        <img src={deck[cardSelected]} alt="" />
+      </div>
+    );
+    setCards(cards);
+  }
+
+  return (
+    <div className="game-container">
+      <div className="card-container player-1">
+        {cards.map((card, key) => (
+          <div className="card-box" key={key}>
+            <div
+              className={`card ${isDragging ? "is-dragging-card" : ""}`}
+              ref={dragRef}
+            >
+              <img src={deck[card]} alt="" />
             </div>
-            <div className="game-dropzone">
-                <div className="dropzone" aria-disabled>
-                </div>
-                <h1>Room title</h1>
-                <div className="dropzone">
-                </div>
-            </div>
-            <div className="card-container player-2">
-            {cardsOponent.map( (card, key) => (
-                    <div className="card-box">
-                        <div className="card">
-                            <img src={deck[card]} alt=""/>
-                        </div>
-                    </div>
-                ))}
-            </div>
+          </div>
+        ))}
+      </div>
+      <div className={`game-dropzone ${isDragging ? "is-dragging-zone" : ""}`}>
+        <div className="dropzone" aria-disabled>
         </div>
-    )
+        <h1>Room title</h1>
+        <div
+          className={`dropzone ${isDragging ? "is-dragging" : ""}`}
+          ref={dropRef}
+        >
+            {playedCard}
+        </div>
+      </div>
+      <div className="card-container player-2">
+        {cardsOponent.map((card, key) => (
+          <div className="card-box">
+            <div className="card">
+              <img src={deck[card]} alt="" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
